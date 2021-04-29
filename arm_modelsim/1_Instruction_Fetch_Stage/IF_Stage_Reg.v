fa4189893e7dc06464547784e0109dcf91f5b16b
Zhivar Sourati <zhivarsourati@Zhivars-MacBook-Pro.local>
2021-04-29 10:01:13 +0430

    Add the instruction fetch stage register

--- arm_modelsim/1_Instruction_Fetch_Stage/IF_Stage_Reg.v
+++ arm_modelsim/1_Instruction_Fetch_Stage/IF_Stage_Reg.v
@@ -3,7 +3,43 @@
 module IF_Stage_Reg(
   input clk, rst, freeze, flush,
   input[`ADDRESS_LEN - 1:0] pc_in, instruction_in,
-  output reg[`ADDRESS_LEN - 1:0] pc, instruction
+  output reg[`ADDRESS_LEN - 1:0] pc_out, instruction_out
+);
+
+wire [`ADDRESS_LEN - 1 : 0] pc;
+wire [`ADDRESS_LEN - 1 : 0] instruction;
+
+// PC register
+Mux # (`ADDRESS_LEN) pc_mux
+(
+    .first(pc_in),
+    .second({`ADDRESS_LEN{1'b0}}),
+    .select(flush),
+    .out(pc)
+);
+Register pc_register
+(
+    .q(pc_out),
+    .d(pc),
+    .freeze(freeze),
+    .clk(clk),
+    .rst(rst)
+);
+// Instruction register
+Mux # (`ADDRESS_LEN) instruction_mux
+(
+    .first(instruction_in),
+    .second({`ADDRESS_LEN{1'b0}}),
+    .select(flush),
+    .out(instruction)
+);
+Register instruction_register
+(
+    .q(instruction_out),
+    .d(instruction),
+    .freeze(freeze),
+    .clk(clk),
+    .rst(rst)
 );
 
 endmodule
