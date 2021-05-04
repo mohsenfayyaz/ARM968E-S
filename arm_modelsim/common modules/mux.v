6c6d446b2962d7885ea2a5ce2161b75d88e8b61c
MohsenFayyaz <mohsen.fayyaz77@gmail.com>
2021-05-05 02:45:38 +0430

    Implement ID_Stage

--- arm_modelsim/common modules/mux.v
+++ arm_modelsim/common modules/mux.v
@@ -1,10 +1,10 @@
 module Mux
 #(parameter WIDTH = 8)
 (
-    input wire [ WIDTH - 1 : 0 ] first,
-    input wire [ WIDTH - 1 : 0 ] second,
+    input wire [WIDTH - 1:0] first,
+    input wire [WIDTH - 1:0] second,
     input select,
-    output wire [ WIDTH - 1 : 0 ] out
+    output wire [WIDTH - 1:0] out
 );
     assign out = (~select) ? first : second;
 endmodule
