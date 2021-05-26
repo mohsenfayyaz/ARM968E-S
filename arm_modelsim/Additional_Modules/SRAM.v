77006de128ea9a49aa59d6f3eb69777d69280593
MohsenFayyaz <mohsen.fayyaz77@gmail.com>
2021-05-26 13:06:52 +0430

    Add SRAM Module

---
+++ arm_modelsim/Additional_Modules/SRAM.v
@@ -0,0 +1,21 @@
+`include "configs.v"
+`timescale 1ns/1ns
+
+module SRAM(
+    input CLK,
+    input RST,
+    input SRAM_WE_N,
+    input [16:0] SRAM_ADDR,  //  17 Bits (was 19)
+    inout [31:0] SRAM_DQ  //  Data 32 Bits
+);
+
+    reg [31:0] memory[0:511]; // 65535
+
+    assign #30 SRAM_DQ = SRAM_WE_N ? memory[SRAM_ADDR]: 32'bz;
+
+    always @(posedge CLK) begin
+        if(~SRAM_WE_N) begin
+            memory[SRAM_ADDR] <= SRAM_DQ;  // write in SRAM
+        end
+    end
+endmodule
--- arm_modelsim/arm_tb.v
+++ arm_modelsim/arm_tb.v
@@ -1,10 +1,11 @@
 module ARM_TB ();
 
-  reg clk = 0, rst = 0;
+  reg clk = 0, rst = 0, sram_clk = 0;
 
   ARM arm(.clk(clk), .rst(rst), .FORWARDING_EN(1'b1));
 
   always #10 clk = ~clk; //50MHz
+  always #20 sram_clk = ~sram_clk; //25MHz
 
   initial begin
     rst = 1;
