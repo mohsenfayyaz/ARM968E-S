8b4a757e380ef4131f6ee1606fbfe381537eb5a1
zhpinkman <zhivarsourati@gmail.com>
2021-06-03 10:38:43 +0430

    Create Cache_Controller.v
    Add Cache_Controller file and its header

---
+++ arm_modelsim/Cache_Controller.v
@@ -0,0 +1,26 @@
+`include "configs.v"
+
+module Cache_Controller(
+  input clk, rst, 
+  
+  
+  // inputs and outputs related to the memory stage
+  input [31:0] address, 
+  input [31:0] writeData,
+  
+  input MEM_R_EN, 
+  input MEM_W_EN,
+  
+  output [31:0] rdata,
+  output ready,
+  
+  // inputs and outputs related to the SRAM
+  output [31:0] sram_address,
+  output [31:0] sram_write_data,
+  
+  output write,
+  input [63:0] sram_read_data, 
+  input sram_ready
+);
+
+endmodule
