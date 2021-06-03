60b7aa404a7f2fa1d1c9cf830a9a7ae9d67092e9
zhpinkman <zhivarsourati@gmail.com>
2021-06-03 13:29:24 +0430

    Update Mem_Stage.v
    Add the Cache Controller module to the mem-stage and the in-between wires

--- arm_modelsim/4_Memory_Stage/Mem_Stage.v
+++ arm_modelsim/4_Memory_Stage/Mem_Stage.v
@@ -11,10 +11,18 @@ module Mem_Stage(
   output [`WORD_LEN - 1:0] memory_out,
   
   output SRAM_WE_N, output [16:0] SRAM_ADDR, inout [31:0] SRAM_DQ, output SRAM_ready  // SRAM BUS & Control Signals
+  //output read // cache ready 
 );
   
+  // the wires in this part relate the cache controller to the sram controller
   assign pc = pc_in;
   wire[3:0] SRAM_ignored_signals;
+  wire [31:0] sram_address;
+  wire [31:0] sram_write_data;
+
+  wire sram_write_en, sram_read_en;
+  wire [31:0] sram_read_data; 
+  wire sram_read;
   
   generate
     if(`USE_SRAM)
@@ -41,6 +49,46 @@ module Mem_Stage(
         .SRAM_CE_N(SRAM_ignored_signals[2]),
         .SRAM_OE_N(SRAM_ignored_signals[3])
       );
+    else if(`USE_CACHE) begin
+      SRAM_Controller sram_controller(
+        .clk(clk), .rst(rst),
+
+        // Golden Inputs
+        .write_en(sram_write_en), .read_en(sram_read_en), // ???????
+        .address(sram_address),
+        .writeData(sram_write_data),
+
+        // WB
+        .readData(sram_read_data),
+
+        // Freeze
+        .ready(sram_ready),
+
+        // SRAM
+        .SRAM_DQ(SRAM_DQ),
+        .SRAM_ADDR(SRAM_ADDR),
+        .SRAM_UB_N(SRAM_ignored_signals[0]),
+        .SRAM_LB_N(SRAM_ignored_signals[1]),
+        .SRAM_WE_N(SRAM_WE_N),
+        .SRAM_CE_N(SRAM_ignored_signals[2]),
+        .SRAM_OE_N(SRAM_ignored_signals[3])
+      );
+      Cache_Controller cache_controller(
+        .clk(clk), .rst(rst), 
+        .address(ALU_Res), 
+        .writeData(Val_Rm), 
+        .MEM_R_EN(MEM_R_EN),
+        .MEM_W_EN(MEM_W_EN),
+        .rdata(memory_out),
+        .ready(ready), 
+        .sram_address(sram_address),
+        .sram_write_data(sram_write_data),
+        .sram_write_en(sram_write_en),
+        .sram_read_en(sram_read_en),
+        .sram_read_data(sram_read_data), 
+        .sram_ready(sram_ready)
+      );
+    end
     else begin
       Data_Memory data_memory(
         .clk(clk), .rst(rst),
